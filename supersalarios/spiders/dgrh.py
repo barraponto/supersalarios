28faa49c7b411a7ebee0ca35a1f7c92c21312a18
Capi Etheriel <barraponto@gmail.com>
2015-07-15 20:56:42 -0300

    working spider

--- supersalarios/spiders/dgrh.py
+++ supersalarios/spiders/dgrh.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
+import csv
+from itertools import islice
+
 import scrapy
 
 
 class DgrhSpider(scrapy.Spider):
     name = "dgrh"
     allowed_domains = ["siarh.unicamp.br"]
-    start_urls = (
-        'http://www.siarh.unicamp.br/',
-    )
+    search_url = ('http://www.siarh.unicamp.br/'
+                  'consultaFuncionario/action/ConsultaFuncionario'
+                  '?nome=&local=&matricula={matricula}')
+    name_xpath = ('//table[@width="750"]/tr[./td[contains(., "{matricula}")]]'
+                  '/td[1]/text()')
+
+    def start_requests(self):
+        with open('./remuneracao-por-bruto.csv') as datafile:
+            for line in islice(csv.reader(datafile), 1, 101):
+                yield scrapy.Request(
+                    self.search_url.format(matricula=line[0]),
+                    meta={'item': {'matricula': line[0],
+                                   'salario_bruto': line[1],
+                                   'salario_liquido': line[5]}})
 
     def parse(self, response):
-        pass
+        item = response.meta['item']
+        text_data = response.xpath(self.name_xpath.format(**item))
+
+        if text_data:
+            item['nome'] = text_data.extract_first().strip()
+            return item
